fcd3c0b9f314551ca9ff55a7718f43d2b3141f91
ARISHA <164086149+Arishayy@users.noreply.github.com>
2025-02-26 22:43:38 +0500

    Add files via upload
    Daily Task 02

---
+++ main.py
@@ -0,0 +1,22 @@
+# 📌 Task 2 – Daily Python Challenge🎉🐍
+
+# 📆 Task Date: 26/02/2025
+
+# 🎯 Mission :
+# You are a robot assistant, and your job is to ask the user their name before starting a secret mission! 🤖💬
+
+# 💡 Instructions :
+# Ask the user for their name using input().
+# Display a cool welcome message like a high-tech AI would! 🚀
+
+
+name = input("What is Your Name?")
+print(f"\n")
+
+print(f" Accessing.....................")
+print(f"\n")
+
+print(f" Access Granted!")
+print(f"\n")
+
+print(f"Welcome to the secret mission {name}")
